31d856639ad0a9dfd79a31612ea382f6e4f009ca
ciro <cirobtsta@gmail.com>
2023-06-21 01:38:31 -0300

    add array.py

--- algomania/estruturas-de-dados/Arrays.py
+++
@@ -1,3 +0,0 @@
-my_array = [1, 2, 3, 4, 'teste', False, True, 0.1]
-    
-print = my_array
---
+++ algomania/estruturas-de-dados/arrays.py
@@ -0,0 +1,5 @@
+ my_array = [1, 2, 3, 4, 'teste', False, True, 0.1]
+
+print(my_array)
+
+print(my_array[6])
