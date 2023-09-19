4ec5333d287a2204f047161eb639a44a586b83bb
ciro <cirobtsta@gmail.com>
2023-09-18 22:53:35 -0300

    add arrays

---
+++ estruturas-de-dados/strings.py
@@ -0,0 +1,23 @@
+s = 'minha primeira string'
+
+s = 'teste' * 10
+s = "teste" 
+s = "A" * 1024
+s = 'b' * 10
+s
+
+ 
+'cirobatista'.replace('batista', 'btsta') #troca
+
+'algoritmo'.upper() #letras maiúsculas
+
+'algoritmo'.startswith('algo') #verifica se o inicio é TRUE ou FALSE
+
+'algoritmo'.endswith('ritmo') #verifica se o término é TRUE ou FALSE
+
+
+
+
+
+
+
